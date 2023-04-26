ca9cad59231cad3efb268e34a96bdc80b0dd7a96
shreenidhi <shreenidhi1757@gmail.com>
2023-04-26 20:04:04 +0530

    ui changes

--- client/src/Components/Dropdown/dropdown.jsx
+++ client/src/Components/Dropdown/dropdown.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 import Select from 'react-select';
 
 function Dropdown(props) {
+    const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      minHeight: '50px',
+      borderRadius: '0'
+    }),
+    valueContainer: (provided) => ({
+      ...provided,
+      height: '55px',
+      borderRadius: '0'
+    }),
+    input: (provided) => ({
+      ...provided,
+      height: '55px',
+      borderRadius: '0',
+    }),
+    menu: (provided) => ({
+      ...provided,
+      marginTop: '2px',
+    }),
+  };
   const data = [
     {
       value: "Nelson Mandela",
@@ -32,8 +53,9 @@ function Dropdown(props) {
   // set value for default selection
 
   return (
-    <div className="dropdown2 relative inline-block text-left w-full">
+    <div className="dropdown2 relative inline-block text-left w-full h-[7vh]">
       <Select
+        styles={customStyles}
         placeholder="Enter delivery location"
         value={data.find(obj => obj.value === props.selectedValue)} // set selected value
         options={data} // set list of the data
